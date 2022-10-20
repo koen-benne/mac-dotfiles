cbf00eceb908368d44500b5fc3dcb13f7117c599
koen-benne <koen.benne@gmail.com>
2022-10-20 14:46:16 +0200

    some config changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,8 @@ set fish_greeting
 fish_add_path /Users/koenbenne/Library/Python/3.9/bin
 fish_add_path '/Users/koenbenne/.platformsh/bin'
 
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+
 # pnpm
 set -gx PNPM_HOME "/Users/koenbenne/Library/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
