66977505cfac1f9b5f8f207dafa8234541a31f5b
koen-benne <koen.benne@gmail.com>
2023-10-25 15:18:08 +0200

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,6 +11,8 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 
 set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border"
 
+ulimit -n 2048
+
 # tmux stuff
 alias to="turbotmux open"
 alias tc="turbotmux close"
@@ -23,9 +25,18 @@ set -gx PNPM_HOME "/Users/koenbenne/Library/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
 
+set -gx CC "/usr/local/bin/gcc-11"
+
 # neovide
 alias nvide "neovide --frame buttonless --multigrid"
 alias lg "lazygit"
 
+# nix
+alias shell="export NIXPKGS_ALLOW_INSECURE=1 && nix shell --impure"
+
 # asdf shi
-source /usr/local/opt/asdf/libexec/asdf.fish
+# source /usr/local/opt/asdf/libexec/asdf.fish
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
